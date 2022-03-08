ea25039422c64ac210cc5e7f825aedc2646949e8
AshokChandraReddyTippaluri <96752472+AshokChandraReddyTippaluri@users.noreply.github.com>
2022-03-08 12:25:19 +0530

    with subject

--- projectmail1.py
+++ projectmail1.py
@@ -1,11 +1,17 @@
+from email import message
 import smtplib
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
+server = smtplib.SMTP('smtp.gmail.com', 587) #we are using gmail server for this server port is 587
 
-server.starttls()
+server.starttls()  #to encryption the message we are using tls (transport layer service)
 
 server.login('sender@gmail.com', 'password')
 
-server.sendmail('sender@gmail.com', 'reciver@gmail.com', 'This Mail sent throught SMTP server by using python')
+subject = 'This mail sent from smtp'
+body = '3rd mail sent throught SMTP protocol by using python '
+
+Message1 = f'Subject: {subject}\n\n{body}'
+
+server.sendmail('sender@gmail.com', 'reciver@gmail.com', Message1)
 
 print('Mail Sent')
